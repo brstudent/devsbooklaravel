183139f1f763876f94eafbbeef7dd150adb4b505
Veronica de Lima <lindapaineira@gmail.com>
2021-01-03 04:18:12 -0300

    User Create
    Create user action

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use App\Models\User;
 
 class AuthController extends Controller
 {
@@ -16,5 +17,54 @@ class AuthController extends Controller
                 'unauthorized'
             ]
         ]);
+    }
+
+    public function create(Request $request) {
+        $array = ['error' => ''];
+
+        $name = $request->input('name');
+        $email = $request->input('email');
+        $password = $request->input('password');
+        $birthdate = $request->input('birthdate');
+
+        if($name && $email && $password && $birthdate) {
+            //Verifica data de nascimento
+            if(strtotime($birthdate) === false) {
+                $array['error'] = 'Data de nascimento inválida!';
+                return $array;
+            }
+
+            //Verifica se o e-mail já existe
+            $emailExists = User::where('email', $email)->count();
+            if($emailExists === 0) {
+                $hash = password_hash($password, PASSWORD_DEFAULT);
+                $newUser = new User();
+                $newUser->name = $name;
+                $newUser->email = $email;
+                $newUser->password = $hash;
+                $newUser->birthdate = $birthdate;
+                $newUser->save();
+
+                $token = auth()->attempt([
+                    'email' => $email,
+                    'password' => $password
+                ]);
+
+                if(!$token) {
+                    $array['error'] = 'Ocorreu um erro!';
+                    return $array;
+                }
+
+                $array['token'] = $token;
+            } else {
+                $array['error'] = 'E-mail já cadastrado!';
+                return $array;
+            }
+        } else {
+            $array['error'] = 'Todos os campos devem ser preenchidos!';
+            return $array;
+        }
+
+        return $array;
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 
 /*
 |--------------------------------------------------------------------------
@@ -18,13 +19,13 @@ Route::get('/ping', function() {
     return ['pong' => true];
 });
 
-/*
+
 Route::get('/401', [AuthController::class, 'unauthorized'])->name('login');
 
 Route::post('/auth/login', [AuthController::class, 'login']);
 Route::post('/auth/logout', [AuthController::class, 'logout']);
 Route::post('/auth/refresh', [AuthController::class, 'refresh']);
-*/
+
 Route::post('/user', [AuthController::class, 'create']);
 /*
 Route::put('/user', [UserController::class, 'update']);
