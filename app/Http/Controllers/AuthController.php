31f8d161e0ee1ef4d4c9031c91bccaa105a1230d
Veronica de Lima <lindapaineira@gmail.com>
2021-01-03 05:27:03 -0300

    Update AuthController.php

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -19,6 +19,48 @@ class AuthController extends Controller
         ]);
     }
 
+    public function unauthorized() {
+        return response()->json(['error'=>'Não autorizado!'], 401);
+    }
+
+    public function login(Request $request) {
+        $array = ['error' => ''];
+
+        $email = $request->input('email');
+        $password = $request->input('password');
+
+        if($email && $password) {
+            $token = auth()->attempt([
+                'email' => $email,
+                'password' => $password
+            ]);
+
+            if(!$token) {
+                $array['error'] = 'E-mail e/ou senha errado!';
+                return $array;
+            }
+
+            $array['token'] = $token;
+            return $array;
+        }
+
+        $array['error'] = 'Dados não enviados!';
+        return $array;
+    }
+
+    public function logout() {
+        auth()->logout();
+        return ['error' => ''];
+    }
+
+    public function refresh() {
+        $token = auth()->refresh();
+        return [
+            'error' => '',
+            'error' => $token
+        ];
+    }
+
     public function create(Request $request) {
         $array = ['error' => ''];
 
