619e1462aab89f49469577aba21311af6b82b386
Veronica de Lima <lindapaineira@gmail.com>
2021-01-06 00:23:46 -0300

    Controller Criation

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,28 @@ use Illuminate\Support\Facades\Route;
 Route::get('/ping', function () {
     return ['pong' => true];
 });
+
+Route::get('/401', [App\Http\Controllers\AuthController::class, 'unauthorized'])->name('login');
+
+Route::post('/auth/login', [App\Http\Controllers\AuthController::class, 'login']);
+Route::post('/auth/logout', [App\Http\Controllers\AuthController::class, 'logout']);
+Route::post('/auth/refresh', [App\Http\Controllers\AuthController::class, 'refresh']);
+
+Route::post('/user', [App\Http\Controllers\AuthController::class, 'create']);
+Route::put('/user', [App\Http\Controllers\UserController::class, 'update']);
+Route::post('/user/avatar', [App\Http\Controllers\UserController::class, 'updateAvatar']);
+Route::post('/user/cover', [App\Http\Controllers\UserController::class, 'updateCover']);
+
+Route::get('/feed', [App\Http\Controllers\FeedController::class, 'read']);
+Route::get('/user/feed', [App\Http\Controllers\FeedController::class, 'userFeed']);
+Route::get('/user/{id}/feed', [App\Http\Controllers\FeedController::class, 'userFeed']);
+
+Route::get('/user', [App\Http\Controllers\UserController::class, 'read']);
+Route::get('/user/{id}', [App\Http\Controllers\UserController::class, 'read']);
+
+Route::post('/feed', [App\Http\Controllers\FeedController::class, 'create']);
+
+Route::post('/post/{id}/like', [App\Http\Controllers\PostController::class, 'like']);
+Route::post('/post/{id}/comment', [App\Http\Controllers\PostController::class, 'comment']);
+
+Route::post('/search', [App\Http\Controllers\SearchController::class, 'search']);
