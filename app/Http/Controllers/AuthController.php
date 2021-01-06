5a7eea6d715d01fe87ab2ebf2057db1529300f6d
Veronica de Lima <lindapaineira@gmail.com>
2021-01-06 00:36:03 -0300

    Controllers
    Configuration Controllers

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,8 +3,18 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Http\Controllers\Controller;
 
 class AuthController extends Controller
 {
-    //
+    public function __construct() {
+        $this->middleware('auth:api', [
+            'except' =>[
+                'login',
+                'create',
+                'unauthorized'
+            ]
+        ]);
+    }
 }
