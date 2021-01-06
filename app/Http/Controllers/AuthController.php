b6f5a99853ee60736125038c7bf75080ce33c4e0
Veronica de Lima <lindapaineira@gmail.com>
2021-01-06 13:16:33 -0300

    AuthController - Modification

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,9 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
-use App\Http\Controllers\Controller;
 
 class AuthController extends Controller
 {
